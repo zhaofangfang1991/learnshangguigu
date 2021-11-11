a7c42d737a25cfbac657b0ab5caa6210105449b4
zhaofangfang1991 <helloworldzff@163.com>
2021-11-11 17:13:14 +0800

    vue学习七之三天

---
+++ workspace1/demo1/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+        <header class="site-header jumbotron">
+        <div class="container">
+            <div class="row">
+            <div class="col-xs-12">
+                <h1>请发表对自己的规划</h1>
+            </div>
+            </div>
+        </div>
+        </header>
+        <div class="container">
+            <Add :addComments="addComments"/>
+            <List :comments="comments" :deleteComments="deleteComments"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import Add from './components/Add.vue'
+import List from './components/List.vue'
+
+export default {
+    data() {
+        return {
+            comments: [
+                {
+                    name: 'Bob',
+                    content: '我是Bob，我来了'
+                },
+                {
+                    name: 'Eob',
+                    content: '我是Eob，我来学vue了'
+                },
+                {
+                    name: 'Xob',
+                    content: '我是Xob，我要年底换工作。全栈+算法，加油！'
+                },
+            ]
+        }
+    },
+    components: {
+        Add, List
+    },
+    methods: {
+        addComments(comment) {
+            this.comments.unshift(comment)
+        },
+
+        deleteComments(index) {
+            this.comments.splice(index, 1)
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ workspace1/demo1/src/components/Add.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="col-md-4">
+        <form class="form-horizontal">
+        <div class="form-group">
+            <label>用户名</label>
+            <input type="text" class="form-control" placeholder="用户名" v-model="name">
+        </div>
+        <div class="form-group">
+            <label>评论内容</label>
+            <textarea class="form-control" rows="6" placeholder="评论内容" v-model="content"></textarea>
+        </div>
+        <div class="form-group">
+            <div class="col-sm-offset-2 col-sm-10">
+            <button type="button" class="btn btn-default pull-right" @click="add">提交</button>
+            </div>
+        </div>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            name: '',
+            content: ''
+        }
+    },
+    props: {
+        addComments: {
+            type: Function,
+            required: true
+        }
+    },
+    methods: {
+        add() {
+            // 获取数据，判断数据的合法性
+            const name = this.name.trim()
+            const content = this.content.trim()
+
+            if (!name || !content) {
+                alert('name or content must not be blank')
+                return
+            }
+
+            // 整合数据
+            const comment = {
+                'name': name,
+                'content': content
+            }
+
+            // 使用方法
+            this.addComments(comment)
+
+
+        }
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
